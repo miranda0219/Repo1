80b89e961d2a4cd195036bc82ffc83b19abb98e5
laboratorio <laboratorio@SC-LAB04-15.ulatina.net>
2019-05-15 18:51:55 -0600

    Creacion de la clase Persona

---
+++ Repo1/src/Modelos/Persona.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Persona {
+    
+    private int cedula;
+    private String nombre;
+    private String apellido;
+    private String genero;
+   private  int telefono;
+   private String email;
+
+    public Persona() {
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+   
+   
+   
+}
